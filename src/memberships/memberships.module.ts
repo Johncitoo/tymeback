fa7ca45e77fc388ee0a5debf99e3f7b61484b375
Johncitoo <juan.contreras03@alumnos.ucn.cl>
2025-12-20 03:02:10 -0300

    fix: Habilitar memberships controller y alinearlo con service

--- src/memberships/memberships.module.ts
+++ src/memberships/memberships.module.ts
@@ -4,12 +4,12 @@ import { Membership } from './entities/membership.entity';
 import { Plan } from '../plans/entities/plan.entity';
 import { User } from '../users/entities/user.entity';
 import { MembershipsService } from './memberships.service';
-// import { MembershipsController } from './memberships.controller'; // DESHABILITADO
+import { MembershipsController } from './memberships.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Membership, Plan, User])],
   providers: [MembershipsService],
-  controllers: [], // MembershipsController - COMENTADO hasta arreglar gymId issues
+  controllers: [MembershipsController],
   exports: [TypeOrmModule, MembershipsService],
 })
 export class MembershipsModule {}
