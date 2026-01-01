ff72f4d574664dc150877020d1ce552797c4d101
Johncitoo <juan.contreras03@alumnos.ucn.cl>
2026-01-01 17:10:43 -0300

    chore: mover scripts temporales a _docs-historicos/scripts

--- add-document-enum.sql
+++
@@ -1,7 +0,0 @@
--- Agregar 'DOCUMENT' al enum file_purpose_enum
--- Este script es seguro de ejecutar múltiples veces
-
-ALTER TYPE file_purpose_enum ADD VALUE IF NOT EXISTS 'DOCUMENT';
-
--- Verificar valores del enum
-SELECT unnest(enum_range(NULL::file_purpose_enum)) AS purpose_values;
--- fix-files-url-nullable.sql
+++
@@ -1,4 +0,0 @@
--- Fix: Hacer que la columna url sea opcional en files
-ALTER TABLE files ALTER COLUMN url DROP NOT NULL;
-
-SELECT 'Columna url ahora es nullable' AS status;
